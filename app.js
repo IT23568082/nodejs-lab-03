e07fad5bd2b48ca219032f4297f6acdaee1b22ad
DasunD2002 <dasun5967@gmail.com>
2026-02-23 22:28:14 +0530

    Solve issues in app.js file

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
 const myModule = require('./my-module.js');
 console.log(myModule.myFunction());
 
-const condition = true;
-
-const myPromise = new Promise((resolve, reject) => {
-  if (condition) {
-    resolve('Success!');
-  } else {
-    reject('Failure!');
-  }
-});
-
 myPromise.then((result) => {
   console.log(result);
 }).catch((error) => {
